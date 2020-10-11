7df6c1fcac4b30b4d366eef54c4b724ae25d3bce
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-11 19:30:08 +0530

    added loading spinner and final touch to project

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loader
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
 
 
 // Fetching quote from API
 async function getQuote() {
+    loading()
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -24,6 +41,8 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop Loader, show Quote
+        complete();
     } catch (error) {
         getQuote();
         console.log('Oops, no quote', error);
